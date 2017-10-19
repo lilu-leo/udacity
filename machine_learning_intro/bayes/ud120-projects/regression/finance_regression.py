f082de92d075d0711e4e084170458a2d805b2529
lilu <li854425934@163.com>
2017-10-19 16:08:06 +0800

    regression test

--- machine_learning_intro/bayes/ud120-projects/regression/finance_regression.py
+++ machine_learning_intro/bayes/ud120-projects/regression/finance_regression.py
@@ -22,6 +22,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
+#features_list = ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -31,6 +32,19 @@ feature_train, feature_test, target_train, target_test = train_test_split(featur
 train_color = "b"
 test_color = "r"
 
+from sklearn import linear_model
+
+reg = linear_model.LinearRegression()
+reg.fit(feature_train, target_train)
+
+print reg.coef_
+print reg.intercept_
+
+score = reg.score(feature_train, target_train)
+print score
+
+score1 = reg.score(feature_test, target_test)
+print score1
 
 
 ### Your regression goes here!
@@ -64,6 +78,13 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+
+print reg.coef_
+print reg.intercept_
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
